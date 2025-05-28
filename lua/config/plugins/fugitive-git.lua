77df2f99d034b566dfaf4d142a952b1f451356c0
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2025-05-28 15:42:39 -0600

    Add fugitive and remove other snack plugins

---
+++ lua/config/plugins/fugitive-git.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      vim.keymap.set("n", "<leader>gf", ":diffget //2<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gh", ":diffget //3<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gm", ":Gvdiffsplit!<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gu", ":diffupdate<CR>", { desc = "Git status" })
+
+
+      vim.keymap.set("n", "<leader>G", ":G<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", { desc = "Git blame" })
+      vim.keymap.set("n", "<leader>gc", ":Git commit<CR>", { desc = "Git commit" })
+      vim.keymap.set("n", "<leader>gp", ":Git push<CR>", { desc = "Git push" })
+      vim.keymap.set("n", "<leader>gl", ":Git pull", { desc = "Git pull" })
+    end,
+  },
+  { "lewis6991/gitsigns.nvim" },
+}
--- lua/config/plugins/snacks.lua
+++ lua/config/plugins/snacks.lua
@@ -3,7 +3,7 @@ return {
   priority = 1000,
   lazy = false,
   opts = {
-    dashboard = { true },
+    notifier = { enabled = true },
     indent = { enabled = true },
     picker = { enabled = true },
   },
@@ -22,12 +22,8 @@ return {
     { "<leader>fr",      function() Snacks.picker.recent() end,                                  desc = "Recent" },
     -- git
     { "<leader>gb",      function() Snacks.picker.git_branches() end,                            desc = "Git Branches" },
-    { "<leader>gl",      function() Snacks.picker.git_log() end,                                 desc = "Git Log" },
     { "<leader>gL",      function() Snacks.picker.git_log_line() end,                            desc = "Git Log Line" },
-    { "<leader>gs",      function() Snacks.picker.git_status() end,                              desc = "Git Status" },
     { "<leader>gS",      function() Snacks.picker.git_stash() end,                               desc = "Git Stash" },
-    { "<leader>gd",      function() Snacks.picker.git_diff() end,                                desc = "Git Diff (Hunks)" },
-    { "<leader>gf",      function() Snacks.picker.git_log_file() end,                            desc = "Git Log File" },
     -- Grep
     { "<leader>sb",      function() Snacks.picker.lines() end,                                   desc = "Buffer Lines" },
     { "<leader>sB",      function() Snacks.picker.grep_buffers() end,                            desc = "Grep Open Buffers" },
@@ -64,20 +60,10 @@ return {
     { "<leader>ss",      function() Snacks.picker.lsp_symbols() end,                             desc = "LSP Symbols" },
     { "<leader>sS",      function() Snacks.picker.lsp_workspace_symbols() end,                   desc = "LSP Workspace Symbols" },
     -- Other
-    { "<leader>z",       function() Snacks.zen() end,                                            desc = "Toggle Zen Mode" },
-    { "<leader>Z",       function() Snacks.zen.zoom() end,                                       desc = "Toggle Zoom" },
-    { "<leader>.",       function() Snacks.scratch() end,                                        desc = "Toggle Scratch Buffer" },
-    { "<leader>S",       function() Snacks.scratch.select() end,                                 desc = "Select Scratch Buffer" },
-    { "<leader>n",       function() Snacks.notifier.show_history() end,                          desc = "Notification History" },
     { "<leader>bd",      function() Snacks.bufdelete() end,                                      desc = "Delete Buffer" },
     { "<leader>cR",      function() Snacks.rename.rename_file() end,                             desc = "Rename File" },
-    { "<leader>gB",      function() Snacks.gitbrowse() end,                                      desc = "Git Browse",               mode = { "n", "v" } },
     { "<leader>gg",      function() Snacks.lazygit() end,                                        desc = "Lazygit" },
     { "<leader>un",      function() Snacks.notifier.hide() end,                                  desc = "Dismiss All Notifications" },
-    { "<c-/>",           function() Snacks.terminal() end,                                       desc = "Toggle Terminal" },
-    { "<c-_>",           function() Snacks.terminal() end,                                       desc = "which_key_ignore" },
-    { "]]",              function() Snacks.words.jump(vim.v.count1) end,                         desc = "Next Reference",           mode = { "n", "t" } },
-    { "[[",              function() Snacks.words.jump(-vim.v.count1) end,                        desc = "Prev Reference",           mode = { "n", "t" } },
     {
       "<leader>N",
       desc = "Neovim News",

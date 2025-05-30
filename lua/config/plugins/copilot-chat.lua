03625b30f2937633724817f864fe975096e0da0d
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2025-05-30 14:18:41 -0600

    Using github/copilot.vim instead copilot.lua

--- lua/config/plugins/copilot-chat.lua
+++ lua/config/plugins/copilot-chat.lua
@@ -25,7 +25,7 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "github/copilot.vim" },
+      "github/copilot.vim",
       { "nvim-lua/plenary.nvim", branch = "master" },
     },
     build = "make tiktoken",

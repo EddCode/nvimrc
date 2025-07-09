2bd75402f34050d4c4fef36e36c2c03e54698166
Edgar Figueroa <edgar.figueroa@yalocontractor.com>
2025-07-09 11:30:47 -0600

    update key mappings for git commands
    Refactor key mappings in the git plugin configuration to improve
    usability and streamline commands. Removed redundant `<CR>` in
    some mappings for cleaner syntax.
    
    Changes:
    - Simplified `<leader>gh`, `<leader>gd`, `<leader>gm`, and `<leader>gu`
      mappings by removing `<CR>` suffix.

--- lua/config/plugins/git.lua
+++ lua/config/plugins/git.lua
@@ -2,11 +2,10 @@ return {
   {
     "tpope/vim-fugitive",
     config = function()
-      vim.keymap.set("n", "<leader>gf<CR>", ":diffget //2<CR>", { desc = "Git status" })
-      vim.keymap.set("n", "<leader>gh<CR>", ":diffget //3<CR>", { desc = "Git status" })
-      vim.keymap.set("n", "<leader>gd<CR>", ":Gvdiffsplit<CR>", { desc = "Git status" })
-      vim.keymap.set("n", "<leader>gm<CR>", ":Gvdiffsplit!<CR>", { desc = "Git status" })
-      vim.keymap.set("n", "<leader>gu<CR>", ":diffupdate<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gh", ":diffget //3<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gm", ":Gvdiffsplit!<CR>", { desc = "Git status" })
+      vim.keymap.set("n", "<leader>gu", ":diffupdate<CR>", { desc = "Git status" })
 
 
       vim.keymap.set("n", "<leader>G", ":G<CR>", { desc = "Git status" })

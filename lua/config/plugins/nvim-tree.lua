a579c3312151df0d3c5f5ed845f35881ae3d78f5
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2025-10-17 20:32:52 -0600

    nvim tree autofocus on file

--- lua/config/plugins/nvim-tree.lua
+++ lua/config/plugins/nvim-tree.lua
@@ -12,6 +12,10 @@ return {
       },
       actions = {
         open_file = { quit_on_open = true }
+      },
+      update_focused_file = {
+        enable = true,
+        update_cwd = false,
       }
     }
   end
